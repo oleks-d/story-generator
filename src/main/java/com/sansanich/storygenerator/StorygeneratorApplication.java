904d75c94eced09ceb33c37deec00378204ac119
olexForte <WtkmGit1!>
2021-11-04 12:39:48 +0200

    Initial commit

---
+++ src/main/java/com/sansanich/storygenerator/StorygeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sansanich.storygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorygeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorygeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sansanich/storygenerator/StorygeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sansanich.storygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorygeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
